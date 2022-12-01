60427f441146f7b19c2b67b2a23b9241f09e2acf
Minh Phan <70899747+mp2209@users.noreply.github.com>
2022-12-01 23:11:34 +0700

    List group user gui

--- src/main/java/com/chatapp/MainApplication.java
+++ src/main/java/com/chatapp/MainApplication.java
@@ -10,12 +10,13 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("HelloView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/admin-group.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Admin");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module com.chatapp.chatappnmjava {
+module ChatApp.nmjava {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.kordamp.ikonli.javafx;
 
     opens com.chatapp to javafx.fxml;
     exports com.chatapp;
-    exports com.chatapp.Controller;
-    opens com.chatapp.Controller to javafx.fxml;
+    opens com.chatapp.controller to javafx.base, javafx.fxml;
+    opens com.chatapp.model to javafx.base, javafx.fxml;
 }
